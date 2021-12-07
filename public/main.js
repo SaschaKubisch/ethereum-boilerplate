d091995635a81fc1801b4286343ad362fda073bc
SaschaKubisch <sascha.fh.kubisch@gmail.com>
2021-12-07 12:52:16 -0400

    Moralis Hello World

---
+++ public/main.js
@@ -0,0 +1,27 @@
+/* Moralis init code */
+const serverUrl = "https://kfmarlu6gwvy.usemoralis.com:2053/server";
+const appId = "FmeRUIU98sfbL0uGydozheJGm3IjfwdATFfz5j9P";
+Moralis.start({ serverUrl, appId });
+
+/* Authentication code */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+    user = await Moralis.authenticate({ signingMessage: "Log in using Moralis" })
+      .then(function (user) {
+        console.log("logged in user:", user);
+        console.log(user.get("ethAddress"));
+      })
+      .catch(function (error) {
+        console(error);
+      });
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
